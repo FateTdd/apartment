3724693568ea2f33716715b6a78ba30179f19736
FateTdd <59795024+FateTdd@users.noreply.github.com>
2020-03-11 17:34:40 +0000

    add forms

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+class FormMixin(object):
+    def get_errors(self):
+        if hasattr(self, 'errors'):
+            errors = self.errors.get_json_data()
+            new_errors = {}
+            for key, message_dicts in errors.items():
+                messages = []
+                for message in message_dicts:
+                    messages.append(message['message'])
+                new_errors[key] = messages
+            return new_errors
+        else:
+            return {}
+
+
+class User(forms.Form, FormMixin):
+    username = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=60)
+    password = forms.CharField(max_length=20, min_length=6)
