23027c3a3b09bd8d45cafd00b32a3404d27d64e8
FateTdd <59795024+FateTdd@users.noreply.github.com>
2020-03-16 21:55:08 +0000

    change apartmentview

--- app/views.py
+++ app/views.py
@@ -6,15 +6,43 @@ import ast
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import User
+from .models import evaluation as evaluationobj
+from .models import apartment, User
 
 
 # Create your views here.
+
 
 def index(request):
     return HttpResponse("Main page!")
 
 def apartmentview(request):
-    return HttpResponse("Apartmentview page!")
+    def apartmentview(request, loginuser, apartmentid=1):
+        favorite = False
+        if loginuser is not None:
+            obj = User.objects.get(username=loginuser)
+            if str(apartmentid) in ast.literal_eval(obj.favorite):
+                favorite = True
+            else:
+                favorite = False
+        data = apartment.objects.filter(id=apartmentid)[0]
+        img_list = ast.literal_eval(data.img)
+        img_index = [x for x in range(len(img_list))][1:]
+        user_comment_list = []
+        user_comment = evaluationobj.objects.filter(apartment_id=apartmentid)
+        for x in user_comment:
+            user_comment_list.append({
+                'user': User.objects.get(uid=x.user_id).username,
+                'commenttext': x.comment
+            })
+        out_data = {'name': data.name, 'price': data.price, 'information': data.information,
+                    'facilities': data.facilities,
+                    'rent_includes': data.rent_includes, 'address': data.address, 'address_link': data.address_link,
+                    'score': data.score, 'comments': data.comments, 'img': img_list, "img_index": img_index
+            , 'range': [1, 2, 3, 4, 5], 'message': False, 'loginuser': loginuser, 'favorite': favorite,
+                    "commenttext": user_comment_list}
+        return render(request, 'apartment/index.html', {"data": out_data})
 
 def apartment(request):
     return HttpResponse("Apartment page!")
