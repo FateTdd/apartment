601cc2861e595a4bd5c627dd83daab2b7ea8a543
FateTdd <59795024+FateTdd@users.noreply.github.com>
2020-03-16 16:54:48 +0000

    creat MYSQL

--- apartment/settings.py
+++ apartment/settings.py
@@ -65,6 +65,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'django.templatetags.static'
+            ]
         },
     },
 ]
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'apartment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apartment',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from shortuuidfield import ShortUUIDField
+
+
+class User(models.Model):
+    uid = ShortUUIDField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    favorite = models.CharField(max_length=200, default='[]')
+
+    def __str__(self):
+        return self.username
+
+
+class apartment(models.Model):
+    name = models.CharField(max_length=50, null=False, db_index=True)
+    img = models.CharField(max_length=2000)
+    price = models.CharField(max_length=50, null=False)
+    information = models.CharField(max_length=500)
+    facilities = models.CharField(max_length=500)
+    rent_includes = models.CharField(max_length=500)
+    address = models.CharField(max_length=500)
+    address_link = models.CharField(max_length=500)
+    score = models.IntegerField()
+    comments = models.TextField(max_length=2000)
+    is_delete = models.BooleanField(default=False)
+
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'apartment'
+
+    def __str__(self):
+        return self.name
+
+
+class evaluation(models.Model):
+    apartment_id = models.IntegerField()
+    user_id = ShortUUIDField()
+    environment = models.IntegerField()
+    staff_service = models.IntegerField()
+    security = models.IntegerField()
+    cost_performance = models.IntegerField()
+    comment = models.CharField(max_length=2000)
+
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'evaluation'
