0cdd43b09cb9fa63c360a3832222cebc281936db
Jinyangg <574772854@qq.com>
2020-03-17 19:40:11 +0000

    creat_apartment_view

--- app/views.py
+++ app/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
-
-# Create your views here.
+import json
 import ast
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from .models import apartment, User
 from .models import evaluation as evaluationobj
+from .forms import User as FUser
+from .forms import ChangeInfo
 
-# Create your views here.
+
+def checkLogin(func):
+    def warpper(request, *args, **kwargs):
+        if request.session.get('user', False):
+            kwargs["loginuser"] = User.objects.get(uid=request.session.get('user'))
+            return func(request, *args, **kwargs)
+        else:
+            kwargs['loginuser'] = None
+            return func(request, *args, **kwargs)
+    return warpper
 
 
 
-def index(request):
-    return HttpResponse("Main page!")
-
+@checkLogin
 def apartmentview(request, loginuser, apartmentid=1):
     favorite = False
     if loginuser is not None:
@@ -39,48 +45,3 @@ def apartmentview(request, loginuser, apartmentid=1):
                 'score': data.score, 'comments': data.comments, 'img': img_list, "img_index": img_index
         , 'range': [1, 2, 3, 4, 5], 'message': False, 'loginuser': loginuser, 'favorite': favorite, "commenttext": user_comment_list}
     return render(request, 'apartment/index.html', {"data": out_data})
-
-def apartment(request):
-    return HttpResponse("Apartment page!")
-
-
-def collection(request):
-    return HttpResponse("collection page!")
-
-def collect(request):
-    return HttpResponse("collect page!")
-
-def cancelcollect(request):
-    return HttpResponse("cancelcollect page!")
-
-def login(request):
-    return HttpResponse("login page!")
-
-def logout(request):
-    return HttpResponse("logout page!")
-
-def evaluation(request):
-    return HttpResponse("evaluation page!")
-
-def evaluate(request):
-    return HttpResponse("evaluate page!")
-
-def registerview(request):
-    return HttpResponse("registerview page!")
-
-def register(request):
-    return HttpResponse("register page!")
-
-
-def userinfo(request):
-    return HttpResponse("userinfo page!")
-
-
-def changepwd(request):
-    return HttpResponse("changepwd page!")
-
-def forgetpwd(request):
-    return HttpResponse("forgetpwd page!")
-
-def search(request):
-    return HttpResponse("search page!")
